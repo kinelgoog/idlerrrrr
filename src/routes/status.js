291b593f027f82685e3d48593841992098095bfe
kinelgoog <kinelgoog@gmail.com>
2025-11-19 22:01:51 +0400

    Delete src directory

--- src/routes/status.js
+++
@@ -1,9 +0,0 @@
-module.exports = (db) => {
-  const router = require('express').Router();
-  router.get('/', async (req, res) => {
-    if (!req.session.userId) return res.status(401).json({ error:'unauth' });
-    const accounts = await db.getSteamAccountsByUser(req.session.userId);
-    res.json({ accounts });
-  });
-  return router;
-};
